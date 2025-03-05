e420b1ae8e77e1e4f978d6a772aa5aa2faee6ea3
Joey Jia <joeychia@outlook.com>
2025-03-04 22:18:56 -0800

    Support versioning strategy for PWA service worker cache

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,67 +1,83 @@
-const CACHE_NAME = 'online-course-v1';
-const urlsToCache = [
+const CACHE_VERSION = '1.0.0';
+const CACHE_NAME = `online-course-v${CACHE_VERSION}-${new Date().toISOString().split('T')[0]}`;
+const STATIC_CACHE = `static-v${CACHE_VERSION}`;
+const DYNAMIC_CACHE = `dynamic-v${CACHE_VERSION}`;
+const STATIC_ASSETS = [
   '/',
   '/index.html',
   '/manifest.json',
   '/favicon.ico',
-  '/icons/*',
-  '/static/js/*',
-  '/static/css/*'
+  '/icons/icon-72x72.png',
+  '/icons/icon-144x144.png'
 ];
+
+// Function to clean up old caches
+const clearOldCaches = async () => {
+  const cacheKeepList = [CACHE_NAME, STATIC_CACHE, DYNAMIC_CACHE];
+  const keyList = await caches.keys();
+  const cachesToDelete = keyList.filter(key => !cacheKeepList.includes(key));
+  return Promise.all(cachesToDelete.map(key => caches.delete(key)));
+};
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    (async () => {
+      const staticCache = await caches.open(STATIC_CACHE);
+      console.log('Caching static assets');
+      await staticCache.addAll(STATIC_ASSETS);
+      // Immediately activate the new service worker
+      await self.skipWaiting();
+    })()
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response;
+    (async () => {
+      // Try to get the response from the static cache first
+      const staticResponse = await caches.match(event.request, { cacheName: STATIC_CACHE });
+      if (staticResponse) {
+        return staticResponse;
+      }
+
+      try {
+        // If not in static cache, try network
+        const networkResponse = await fetch(event.request);
+        
+        // Cache successful responses in the dynamic cache
+        if (networkResponse.ok && event.request.method === 'GET') {
+          const dynamicCache = await caches.open(DYNAMIC_CACHE);
+          dynamicCache.put(event.request, networkResponse.clone());
         }
-
-        return fetch(event.request).then(
-          (response) => {
-            // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // Clone the response
-            const responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then((cache) => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
+        
+        return networkResponse;
+      } catch (error) {
+        // If network fails, try dynamic cache
+        const cachedResponse = await caches.match(event.request, { cacheName: DYNAMIC_CACHE });
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        throw error;
+      }
+    })()
   );
 });
 
 self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    (async () => {
+      // Clear old caches
+      await clearOldCaches();
+      // Take control of all clients
+      await self.clients.claim();
+      console.log('Service Worker activated and controlling all clients');
+    })()
   );
+});
+
+// Listen for version update messages
+self.addEventListener('message', (event) => {
+  if (event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
